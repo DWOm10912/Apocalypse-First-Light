25d120c6db75588291067db8ba2dc618aa2837c0
willi <william20040622@gmail.com>
2026-08-16 05:34:50 +0900

    Initialize project and integrate TaCZ

---
+++ src/main/java/com/antaurora/apofirstlight/ApocalypseFirstLight.java
@@ -0,0 +1,12 @@
+package com.antaurora.apofirstlight;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(ApocalypseFirstLight.MOD_ID)
+public class ApocalypseFirstLight {
+    public static final String MOD_ID = "apocalypse_firstlight";
+
+    public ApocalypseFirstLight() {
+        // Minimal bootstrap. Gameplay systems will be added in later stages.
+    }
+}
